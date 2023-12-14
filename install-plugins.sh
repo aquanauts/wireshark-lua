c1c39cf01abefc308f99c76e008683a41fb9b05b
Matt Godbolt <matthew.godbolt@aquatic.com>
2023-12-14 15:37:05 -0600

    Dawg

--- install-plugins.sh
+++ install-plugins.sh
@@ -6,4 +6,4 @@ WIRESHARK_PLUGIN_DIR=~/.local/lib/wireshark/plugins
 
 mkdir -p ${WIRESHARK_PLUGIN_DIR}
 
-ln -sf $(pwd)/Nasdaq/Nasdaq.U{q,t}* $(pwd)/Siac/Siac.C{q,t}* $(pwd)/Nasdaq/*.Itch.*.lua $(pwd)/Nyse/*Integ*.lua ${WIRESHARK_PLUGIN_DIR}
+ln -sf $(pwd)/Nasdaq/Nasdaq.U{q,t}* $(pwd)/Nasdaq/*Total*.lua $(pwd)/Siac/Siac.C{q,t}* $(pwd)/Nasdaq/*.Itch.*.lua $(pwd)/Nyse/*Integ*.lua ${WIRESHARK_PLUGIN_DIR}
